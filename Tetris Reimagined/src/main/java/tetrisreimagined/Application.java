782b8c2152686da8066c7fb19f72a2988bfa554c
TiagooGomess <tiagogomes2011@live.com.pt>
2020-04-19 19:52:26 +0100

    Initial program architecture

---
+++ Tetris Reimagined/src/main/java/tetrisreimagined/Application.java
@@ -0,0 +1,8 @@
+package tetrisreimagined;
+
+public class Application {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.run();
+    }
+}
---
+++ Tetris Reimagined/src/main/java/tetrisreimagined/Game.java
@@ -0,0 +1,7 @@
+package tetrisreimagined;
+
+public class Game {
+    public void run() {
+        System.out.println("Running game...");
+    }
+}
