ef8bcfe91dbdbf0f4d199a207e88fd70fa5ee99c
TiagooGomess <tiagogomes2011@live.com.pt>
2020-04-20 14:53:54 +0100

    Renamed class

---
+++ Tetris Reimagined/src/main/java/tetrisreimagined/play/view/lanterna/GameViewLanterna.java
@@ -0,0 +1,23 @@
+package tetrisreimagined.play.view.lanterna;
+
+import com.googlecode.lanterna.screen.Screen;
+
+import java.io.IOException;
+
+public class GameViewLanterna {
+
+    private Screen screen;
+    private int width, height;
+
+    public GameViewLanterna(Screen screen, int width, int height) {
+        this.screen = screen;
+        this.width = width;
+        this.height = height;
+
+    }
+
+    public void drawAll() throws IOException {
+
+    }
+
+}
