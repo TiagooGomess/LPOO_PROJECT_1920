c20cd4348cb1ba57e378ac262712f57237a75a56
TiagooGomess <tiagogomes2011@live.com.pt>
2020-04-20 11:35:18 +0100

    Added lanterna and mockito to build.gradle

---
+++ Tetris Reimagined/src/main/java/tetrisreimagined/play/view/lanterna/DrawGameLanterna.java
@@ -0,0 +1,11 @@
+package tetrisreimagined.play.view.lanterna;
+
+import com.googlecode.lanterna.screen.Screen;
+
+public class DrawGameLanterna {
+    private Screen screen;
+
+    public DrawGameLanterna(Screen screen) {
+        this.screen = screen;
+    }
+}
