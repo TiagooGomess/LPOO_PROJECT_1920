452eb1f3e3b59b60c2928c1fc900e48f8d2fc1c4
TiagooGomess <tiagogomes2011@live.com.pt>
2020-04-30 14:04:18 +0100

    ColorTest done

--- Tetris Reimagined/src/test/java/model/ColorTest.java
+++ Tetris Reimagined/src/test/java/model/ColorTest.java
@@ -3,7 +3,6 @@ package model;
 import org.junit.Before;
 import org.junit.Test;
 import tetrisreimagined.play.model.Color;
-import tetrisreimagined.play.model.Position;
 
 import java.util.ArrayList;
 import java.util.List;
